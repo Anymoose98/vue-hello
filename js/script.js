5104cdabda4720f9256c39864c6efc6a83387118
Anymoose98 <daniel.digiacomo98@gmail.com>
2023-12-12 18:00:42 +0100

    bonus fatto

--- js/script.js
+++ js/script.js
@@ -3,7 +3,8 @@ const { createApp } = Vue
 createApp({
   data() {
     return {
-      saluta: 'Hello Vue!'
+      saluta: 'Hello Vue!',
+      foto: "https://www.ufficistampanazionali.it/wp-content/uploads/2023/02/studio-proporzione-aurea.jpg",
     }
   }
 }).mount('#app')
